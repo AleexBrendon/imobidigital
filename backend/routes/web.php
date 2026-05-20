3bbc16790ca82b8547adcb7da47393d6564572e4
AleexBrendon <alexbrendo.dev@gmail.com>
2026-05-20 20:49:18 -0300

    router temp

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/run-migrate-secret-123', function () {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('migrate', [
+        '--force' => true,
+    ]);
+
+    return nl2br(Artisan::output());
 });
