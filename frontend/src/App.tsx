cca0a4ef29db4c187163949874bc2e861d5e4f47
AleexBrendon <alexbrendo.dev@gmail.com>
2026-05-20 00:57:53 -0300

    ajust app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AppRoutes } from "./routes/AppRoutes";
 import { PublicBotWidget } from "./components/bot/PublicBotWidget";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppRoutes />
       <PublicBotWidget />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
