59665a9181eb2a5c461f39b84ac480615ed10bf7
AleexBrendon <alexbrendo.dev@gmail.com>
2026-05-20 22:14:47 -0300

    remove temporary migrate route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -9,16 +9,6 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/run-migrate-secret-123', function () {
-    Artisan::call('config:clear');
-    Artisan::call('cache:clear');
-    Artisan::call('migrate', [
-        '--force' => true
-    ]);
-
-    return '<pre>' . Artisan::output() . '</pre>';
-});
-
 Route::get('/debug-error', function () {
     return response()->json([
         'app' => config('app.name'),
